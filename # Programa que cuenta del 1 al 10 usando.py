c9a9365fe06522ae1df26bcb2e0c2964ba9e4074
Sofiavanegas1 <135055849+Sofiavanegas1@users.noreply.github.com>
2023-05-30 12:56:10 -0500

    Create # Programa que cuenta del 1 al 10 usando.py

---
+++ # Programa que cuenta del 1 al 10 usando.py
@@ -0,0 +1,11 @@
+# Programa que cuenta del 1 al 10 usando un bucle while
+
+# Inicializamos la variable de conteo en 1
+contador = 1
+
+# Mientras el contador sea menor o igual a 10
+while contador <= 20:
+    print(contador)  # Imprimimos el valor del contador
+    contador += 1  # Incrementamos el contador en 1 en cada iteración
+
+# Fin del bucle while
