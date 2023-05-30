db26a4b76fcd3d9d39ae157765feb61954c40cb1
Sofiavanegas1 <135055849+Sofiavanegas1@users.noreply.github.com>
2023-05-30 12:56:26 -0500

    Create # Programa que imprime los números pares.py

---
+++ # Programa que imprime los números pares.py
@@ -0,0 +1,8 @@
+# Programa que imprime los números pares del 1 al 10 usando un bucle for
+
+# Utilizamos el bucle for para iterar sobre los números del 1 al 10
+for i in range(1, 101):
+    if i % 2 == 0:  # Verificamos si el número es par
+        print(i)  # Imprimimos el número si es par
+
+# Fin del bucle for
